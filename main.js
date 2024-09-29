042f2108920cd91ae8128b22c7e187ae9213d0da
Alexandre Castejon <alexandrecastejon@hotmail.com>
2024-09-29 12:38:43 -0300

    commit inicial

---
+++ main.js
@@ -0,0 +1,17 @@
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    
+    const name = document.getElementById('name').value;
+    const phone = document.getElementById('phone').value;
+    
+    const table = document.getElementById('contactsTable').getElementsByTagName('tbody')[0];
+    const newRow = table.insertRow();
+    
+    const nameCell = newRow.insertCell(0);
+    const phoneCell = newRow.insertCell(1);
+    
+    nameCell.textContent = name;
+    phoneCell.textContent = phone;
+    
+    document.getElementById('contactForm').reset();
+});
